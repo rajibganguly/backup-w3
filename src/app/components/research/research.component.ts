210fa3a590b6c757fc48aba653b84058892f8aa5
Rajib Ganguly <onezerodesigns@gmail.com>
2021-02-03 22:27:51 +0530

    added loading image & resolve research bug

--- src/app/components/research/research.component.ts
+++ src/app/components/research/research.component.ts
@@ -11,7 +11,20 @@ import { W3esearviceMainService } from 'src/app/services/w3esearvice-main.servic
 })
 export class ResearchComponent implements OnInit {
   @ViewChild('selectRashi', {static: true}) selectRashi: ElementRef;
-  zodac: any[] = ['Mar 19 - Apr 20', 'Apr 19 - May 20', 'May 19 - Jun 20', 'Jun 19 - Jul 20', 'Jul 19 - Aug 20', 'Aug 19 - Sep 20', 'Sep 19 - Oct 20', 'Oct 19 - Nov 20', 'Nov 19 - Dec 20',  'Dec 19 - Jan 20', 'Jan 19 - Feb 20', 'Feb 19 - Mar 20'];
+  zodac: any[] = [
+    {value: 'aries', name: 'Mar 19 - Apr 20'}, 
+    {value: 'taurus', name: 'Apr 19 - May 20'},  
+    {value: 'gemini', name: 'May 19 - Jun 20'},  
+    {value: 'cancer', name: 'Jun 19 - Jul 20'},  
+    {value: 'leo', name: 'Jul 19 - Aug 20'},  
+    {value: 'virgo', name: 'Aug 19 - Sep 20'},  
+    {value: 'libra', name: 'Sep 19 - Oct 20'},  
+    {value: 'scorpio', name: 'Oct 19 - Nov 20'},  
+    {value: 'sagittarius', name: 'Nov 19 - Dec 20'},   
+    {value: 'capricorn', name: 'Dec 19 - Jan 20'},  
+    {value: 'aquarius', name: 'Jan 19 - Feb 20'},  
+    {value: 'pisces', name: 'Feb 19 - Mar 20'}
+  ];
   dataDetails;
   homeReturn = true;
   loader = false;
